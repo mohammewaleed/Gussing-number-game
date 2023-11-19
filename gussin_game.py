c61eb244a8c7a7c4425de3b5ae71911877154472
mohammewaleed <103078439+mohammewaleed@users.noreply.github.com>
2023-11-19 22:02:13 +0200

    Tranning

--- gussin_game.py
+++ gussin_game.py
@@ -1,20 +1,24 @@
 import random2;
+while True:
+  pc_num = random2.randint(1,10)
+  print(pc_num)
+  attemsps = 0
 
-pc_num = random2.randint(1,10)
-print(pc_num)
-attemsps = 0
-
-while True: 
-  player_num = int(input("Pick a number between 1-10: "))
-  if 10 >= player_num >= 1:
-    attemsps+=1
-  else:
-    player_num = input("It is invaled,number between 1-10: ")
-
-  if player_num == pc_num:
-    print("Your are won in " + str(attemsps) + " try")
+  while True: 
+    player_num = int(input("Pick a number between 1-10: "))
+    if 10 >= player_num >= 1:
+      attemsps+=1
+    else:
+      player_num = input("It is invaled,number between 1-10: ")
+      continue
+    if player_num == pc_num:
+      print("Your are won in " + str(attemsps) + " try")
+      break
+    elif player_num > pc_num:
+        print("Your number is greater than pc number.")
+    else:
+      print("Your number is less than pc number.")
+  play_again = input("Do you want to play again?").lower()
+  if play_again != "yes":
+    print("Thanks, we want to see you again.")
     break
-  elif player_num > pc_num:
-      print("Your number is greater than pc number.")
-  else:
-    print("Your number is less than pc number.")
